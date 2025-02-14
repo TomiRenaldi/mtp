3604a3f660a6091f0d21ff0d0b6e37624778a49f
TomiRenaldi <tomirenaldi2@gmail.com>
2025-02-14 14:14:22 +0700

    Konsep mengubah UI keseluruhan

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        <Navbar />
+        <Header />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
